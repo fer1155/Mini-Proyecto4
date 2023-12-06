56456d53cd28ecc8c75d67150c587b65f24b98db
Fernando Cardona <cardona.fernando@correounivalle.edu.co>
2023-12-06 18:52:03 -0500

    Ventana admin

--- Documents/NetBeansProjects/Miniproyecto No.4/src/controlador/AdminPrincipalController.java
+++ Documents/NetBeansProjects/Miniproyecto No.4/src/controlador/AdminPrincipalController.java
@@ -1,5 +1,9 @@
 package controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import modelo.AdminPrincipalModel;
 import vista.AdminPrincipalView;
 
@@ -11,5 +15,54 @@ public class AdminPrincipalController {
         this.vista = vista;
         this.modelo = modelo;
         vista.setVisible(true);
+        this.vista.addBtonClienteListener(new accionesMouseBton1());
+        this.vista.addBtonbton2Listener(new accionesMouseBton2());
+        this.vista.addBtonbton3Listener(new accionesMouseBton3());
+        this.vista.addBtonbton4Listener(new accionesMouseBton4());
+        this.vista.addBtonClienteActionListener(new acciones());
+        this.vista.addBtonBton2ClienteActionListener(new acciones());
+    }
+    
+    class accionesMouseBton1 extends MouseAdapter{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            vista.setButtonBoder(vista.getjButton1());
+        }
+    }
+    
+    class accionesMouseBton2 extends MouseAdapter{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            vista.setButtonBoder(vista.getjButton2());
+        }
+    }
+    
+    class accionesMouseBton3 extends MouseAdapter{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            vista.setButtonBoder(vista.getjButton4());
+        }
+    }
+    
+    class accionesMouseBton4 extends MouseAdapter{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            vista.setButtonBoder(vista.getjButton5());
+        }
+    }
+    
+    class acciones implements ActionListener{ 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getActionCommand().equalsIgnoreCase("Clientes")){
+                System.out.println("Hola");
+                vista.mostrarPestanaClientes();
+            }
+            
+            if(e.getActionCommand().equalsIgnoreCase("#2")){
+                System.out.println("Hola 2");
+                vista.mostrarPestana2();
+            }
+        }
     }
 }
--- Documents/NetBeansProjects/Miniproyecto No.4/src/vista/AdminPrincipalView.java
+++ Documents/NetBeansProjects/Miniproyecto No.4/src/vista/AdminPrincipalView.java
@@ -4,6 +4,14 @@
  */
 package vista;
 
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.border.Border;
+
 /**
  *
  * @author Fernando
@@ -13,8 +21,32 @@ public class AdminPrincipalView extends javax.swing.JFrame {
     /**
      * Creates new form AdminPrincipalView
      */
+    
+    //create border
+    Border defaul_border = BorderFactory.createMatteBorder(0, 0, 3, 0, new Color(153,153,153));
+    Border red_border = BorderFactory.createMatteBorder(0, 0, 3, 0, Color.red);
+
+    //create an array of Jbuttons
+    JButton[] buttons;
+    
     public AdminPrincipalView() {
         initComponents();
+        
+        //center the form
+        //this.setLocationRelativeTo(null);
+        
+        //add buttons to the array
+        buttons = new JButton[4];
+        buttons[0] = jButton1;
+        buttons[1] = jButton2;
+        buttons[2] = jButton4;
+        buttons[3] = jButton5;
+        
+        //add border to the jbuttons
+        for(JButton button : buttons){
+            button.setBorder(defaul_border);
+        }
+       
     }
 
     /**
@@ -27,44 +59,167 @@ public class AdminPrincipalView extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        content = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("ADMIN");
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        jButton1.setFont(new java.awt.Font("Verdana", 0, 13)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(153, 153, 153));
+        jButton1.setText("Clientes");
+        jButton1.setBorder(null);
+        jButton1.setContentAreaFilled(false);
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.setFocusPainted(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setFont(new java.awt.Font("Verdana", 0, 13)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(153, 153, 153));
+        jButton2.setText("#4");
+        jButton2.setBorder(null);
+        jButton2.setContentAreaFilled(false);
+        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton2.setFocusPainted(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setFont(new java.awt.Font("Verdana", 0, 13)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(153, 153, 153));
+        jButton4.setText("#2");
+        jButton4.setBorder(null);
+        jButton4.setContentAreaFilled(false);
+        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton4.setFocusPainted(false);
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setFont(new java.awt.Font("Verdana", 0, 13)); // NOI18N
+        jButton5.setForeground(new java.awt.Color(153, 153, 153));
+        jButton5.setText("#3");
+        jButton5.setBorder(null);
+        jButton5.setContentAreaFilled(false);
+        jButton5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton5.setFocusPainted(false);
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36)
+                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(172, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        content.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout contentLayout = new javax.swing.GroupLayout(content);
+        content.setLayout(contentLayout);
+        contentLayout.setHorizontalGroup(
+            contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        contentLayout.setVerticalGroup(
+            contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 368, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(66, 66, 66)
-                .addComponent(jLabel1)
-                .addContainerGap(429, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(content, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(116, 116, 116)
-                .addComponent(jLabel1)
-                .addContainerGap(168, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(48, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(content, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    //create a unction to set border to the selectd menu itemn
+    public void setButtonBoder(JButton boton){
+        for(JButton button : buttons){
+            button.setBorder(defaul_border);
+            button.setForeground(new Color(153, 153, 153));
+        }
+        
+        boton.setBorder(red_border);
+        boton.setForeground(Color.black);
+    }
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -102,7 +257,74 @@ public class AdminPrincipalView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel content;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
+    
+    public void addBtonClienteListener(MouseListener listenControl){
+        jButton1.addMouseListener(listenControl);
+    }
+    
+    public void addBtonbton2Listener(MouseListener listenControl){
+        jButton4.addMouseListener(listenControl);
+    }
+    
+    public void addBtonbton3Listener(MouseListener listenControl){
+        jButton5.addMouseListener(listenControl);
+    }
+    
+    public void addBtonbton4Listener(MouseListener listenControl){
+        jButton2.addMouseListener(listenControl);
+    }
+    
+    public void addBtonClienteActionListener(ActionListener listenControl){
+        jButton1.addActionListener(listenControl);
+    }
+    
+    public void addBtonBton2ClienteActionListener(ActionListener listenControl){
+        jButton4.addActionListener(listenControl);
+    }
+    
+    public JButton getjButton1() {
+        return jButton1;
+    }
+
+    public JButton getjButton2() {
+        return jButton4;
+    }
+
+    public JButton getjButton4() {
+        return jButton5;
+    }
+
+    public JButton getjButton5() {
+        return jButton2;
+    }
+    
+    public void mostrarPestanaClientes(){
+        Page1 pg1 = new Page1();
+        pg1.setSize(720, 368);
+        pg1.setLocation(0, 0);
+        
+        content.removeAll();
+        content.add(pg1);
+        content.revalidate();
+        content.repaint();
+    }
+    
+    public void mostrarPestana2(){
+        Page2 pg2 = new Page2();
+        pg2.setSize(720, 368);
+        pg2.setLocation(0, 0);
+        
+        content.removeAll();
+        content.add(pg2);
+        content.revalidate();
+        content.repaint();
+    }
 }
